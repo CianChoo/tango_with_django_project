5cf642ce2f41e3616a1231a9821924f268cfc297
Cian Choo <2620317C@student.gla.ac.uk>
2023-01-19 18:01:02 +0000

    Ch4 Commit

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    message = "Rango says hey there partner! " + '<a href="/rango/about">About</a>'
-    return HttpResponse(message)
-    
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
-    message = "Rango says here is the about page. " + '<a href="/rango/">Index</a>'
-    return HttpResponse(message)
+    context_dict = {'message': 'This tutorial has been put together by Cian'}
+    return render(request, 'rango/about.html', context=context_dict)
